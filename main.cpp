9b8c134eadf7c0052d203bb71871cf0362ec94af
zzuhis <zzuhis@gmail.com>
2022-02-24 14:53:02 +0200

    Added cli operand

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "Stack.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     /*
 Post: The program has executed simple arithmetic
       commands entered by the user.
@@ -8,12 +8,14 @@ Uses: The class Stack and the functions
       introduction, instructions, do_command, and get_command.
 */
 
-    {
+
         Stack stored_numbers;
-        introduction();
-        instructions();
-        while (stored_numbers.do_command(stored_numbers.get_command(), stored_numbers));
-    }
+        std::string line = "-p";
+        if (argv[1] == line) {
+            introduction();
+            instructions();
+            while (stored_numbers.do_command(stored_numbers.get_command(), stored_numbers));
+        }
 
     return 0;
 }
